e063886bda3d36badde99a80a7c237f8eb15849c
Straightpool <42584250+Straightpool@users.noreply.github.com>
2019-12-22 14:41:22 +0100

    Update stuck_check.sh

--- stuck_check.sh
+++ stuck_check.sh
@@ -4,14 +4,14 @@
 
 while true
 do
-  LastLog=`journalctl -u shelleypoold.service | tail -n 1`
+  LastLog=`journalctl -u <SHELLEYSERVICENAME>.service | tail -n 1`
 
   if [[ $(echo $LastLog | grep 'stuck_notifier') ]]; then
-    Uptime=`/home/sl/.cargo/bin/jcli rest v0 node stats get -h http://127.0.0.1:9099/api | grep 'uptime'`
-    echo "Restarting due to stuck_notifier at $Uptime" >> /home/sl/logs/stuck_check.log
-    systemctl stop shelleypoold
+    Uptime=`/home/<USER>/.cargo/bin/jcli rest v0 node stats get -h http://127.0.0.1:<REST_PORT>/api | grep 'uptime'`
+    echo "Restarting due to stuck_notifier at $Uptime" >> /home/<USER>/logs/stuck_check.log
+    systemctl stop <SHELLEYSERVICENAME>
     sleep 5
-    systemctl start shelleypoold
+    systemctl start <SHELLEYSERVICENAME>
   fi
 
 sleep 10
