e5423a8c0f1985fc6dbd9ec4610cf76f6d2ea058
Straightpool <42584250+Straightpool@users.noreply.github.com>
2019-12-25 18:38:44 +0100

    Update stuck_check.sh

--- stuck_check.sh
+++ stuck_check.sh
@@ -7,16 +7,16 @@
 # Run script with sudo rights, to put into the background at the same time: nohup sudo ./stuck_check.sh 2>&1 &
 # If you want the script to restart your node on other log keywords but stuck_notifier replace optionalsecondkeyword with the keyword you want to check for
 
-journalctl -u <SHELLEY>.service -n 1 -f | grep --line-buffered 'stuck_notifier\|optionalsecondkeyword' | while read LINE
+journalctl -u <SHELLEYD>.service -n 1 -f | grep --line-buffered 'stuck_notifier\|optionalsecondkeyword' | while read LINE
 do
   if [[ -n $LINE ]]; then
     NOW=$(date +"%Y-%m-%d_%H:%M:%S_%Z")  
     Uptime=`/home/<USER>/.cargo/bin/jcli rest v0 node stats get -h http://127.0.0.1:<REST_PORT>/api | grep 'uptime'`
     LastBlockHeight==`/home/<USER>/.cargo/bin/jcli rest v0 node stats get -h http://127.0.0.1:<REST_PORT>/api | grep 'lastBlockHeight'`
     echo "Restarting due to stuck_notifier with $Uptime at $LastBlockHeight at $NOW" >> /home/<USER>/logs/stuck_check.log
-    systemctl stop shelleypoold
+    systemctl stop <SHELLEYD>
     sleep 5
-    systemctl start shelleypoold
+    systemctl start <SHELLEYD>
   fi
 
 sleep 60
