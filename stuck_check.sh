9f860112743ce0ecd256a2df40a4b857462602de
Straightpool <42584250+Straightpool@users.noreply.github.com>
2019-12-26 13:08:03 +0100

    Update stuck_check.sh

--- stuck_check.sh
+++ stuck_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Basic StakePool Node Restart script after stuck_notifier is received
 # Created by Straight Pool, Ticker STR8
-# Script version: 2.1
+# Script version: 2.1.1
 
 # This script assumes you have setup your pool as a systemd service
 # Run script with sudo rights, to put into the background at the same time: nohup sudo ./stuck_check.sh 2>&1 &
@@ -12,7 +12,7 @@ do
   if [[ -n $LINE ]]; then
     NOW=$(date +"%Y-%m-%d_%H:%M:%S_%Z")  
     Uptime=`/home/<USER>/.cargo/bin/jcli rest v0 node stats get -h http://127.0.0.1:<REST_PORT>/api | grep 'uptime'`
-    LastBlockHeight==`/home/<USER>/.cargo/bin/jcli rest v0 node stats get -h http://127.0.0.1:<REST_PORT>/api | grep 'lastBlockHeight'`
+    LastBlockHeight=`/home/<USER>/.cargo/bin/jcli rest v0 node stats get -h http://127.0.0.1:<REST_PORT>/api | grep 'lastBlockHeight'`
     echo "Restarting due to stuck_notifier with $Uptime at $LastBlockHeight at $NOW" >> /home/<USER>/logs/stuck_check.log
     systemctl stop <SHELLEYD>
     sleep 5
