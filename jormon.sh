32e250953a115b517867dc7cd3bddf982f40f729
Straightpool <42584250+Straightpool@users.noreply.github.com>
2020-01-01 22:48:17 +0100

    Update jormon.sh
    Added uptime to logging, modified timing of tolerances

--- jormon.sh
+++ jormon.sh
@@ -8,15 +8,18 @@
 # threshold. The script does NOT perform a restart on the Jormungandr node. Instead we rely on process 
 # managers such as systemd to perform restarts.
 #
-# Modification: The script also considers a node stuck if it is too long offline or in bootstrap mode. It then uses systemd 
-# to actually restart the node, as a simple shutdown won't work in these scenarios
-# Run the script with sudo rights to make use of this added functionality
-#
-# Version 2.1
-
+# Modifications: 
+# - The script also considers a node stuck if it is too long offline or in bootstrap mode. It then uses systemd 
+#   to actually restart the node, as a simple shutdown won't work in these scenarios
+#   Run the script with sudo rights to make use of this added functionality
+# - Added uptime to logging output
+# - Modified timing of SYNC_TOLERANCE_SECONDS from 240 to 300 seconds
+ 
+# Version 2.2
 
 POLLING_INTERVAL_SECONDS=30
-SYNC_TOLERANCE_SECONDS=240
+SYNC_TOLERANCE_SECONDS=300
+BOOTSTRAP_TOLERANCE_SECONDS=480
 REST_API="http://127.0.0.1:<REST_PORT>/api"
 BOOTSTRAP_TIME=$SECONDS
 
@@ -25,13 +28,13 @@ while true; do
     LAST_BLOCK=$(jcli rest v0 node stats get --output-format json --host $REST_API 2> /dev/null)
     LAST_BLOCK_HEIGHT=$(echo $LAST_BLOCK | jq -r .lastBlockHeight)
     LAST_BLOCK_DATE=$(echo $LAST_BLOCK | jq -r .lastBlockTime)
+    UPTIME=$(echo $LAST_BLOCK | jq -r .uptime)
     LAST_BLOCK_TIME=$(date -d$LAST_BLOCK_DATE +%s 2> /dev/null)
     CURRENT_TIME=$(date +%s)
     DIFF_SECONDS=$((CURRENT_TIME - LAST_BLOCK_TIME))
-
     if ((LAST_BLOCK_TIME > 0)); then
         if ((DIFF_SECONDS > SYNC_TOLERANCE_SECONDS)); then
-            echo "Jormungandr out-of-sync. Time difference of $DIFF_SECONDS seconds. Shutting down node..."
+            echo "Jormungandr out-of-sync. Time difference of $DIFF_SECONDS seconds. Shutting down node with uptime $UPTIME..."
             jcli rest v0 shutdown get --host $REST_API
             BOOTSTRAP_TIME=$SECONDS
         else
@@ -41,7 +44,7 @@ while true; do
     else
         BOOTSTRAP_ELAPSED_TIME=$(($SECONDS - $BOOTSTRAP_TIME))
         echo "Jormungandr node is offline or bootstrapping since $BOOTSTRAP_ELAPSED_TIME..."
-        if ((BOOTSTRAP_ELAPSED_TIME > SYNC_TOLERANCE_SECONDS)); then
+        if ((BOOTSTRAP_ELAPSED_TIME > BOOTSTRAP_TOLERANCE_SECONDS)); then
           echo "Jormungandr stuck in bootstrap or offline too long. Attempting to restart node..."
           systemctl stop <jormungandr.service>
           sleep 5
