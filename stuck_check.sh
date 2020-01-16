3b478f69209f9dc390e9f9960ee86a5b8df454dc
Straightpool <42584250+Straightpool@users.noreply.github.com>
2020-01-16 21:03:59 +0100

    Update stuck_check.sh

--- stuck_check.sh
+++ stuck_check.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 # Basic StakePool Node Restart script after stuck_notifier is received
-# Created by Straightpool (https://straightpool.github.io/about/) 
-# Script version: 2.1.1
+# Created by Straight Pool, Ticker STR8
+# Script version: 2.3
 
 # This script assumes you have setup your pool as a systemd service
-# Run script with sudo rights, to put into the background at the same time: nohup sudo ./stuck_check.sh 2>&1 &
 # If you want the script to restart your node on other log keywords but stuck_notifier replace optionalsecondkeyword with the keyword you want to check for
+#
+# In node-config you can define the time threshold when you want the node to throw the stuck_notifier with e.g.: 
+# no_blockchain_updates_warning_interval: 5m
+# Default is 30m
 
-journalctl -u <SHELLEYD>.service -n 1 -f | grep --line-buffered 'stuck_notifier\|optionalsecondkeyword' | while read LINE
+REST_API="http://127.0.0.1:<REST_PORT>/api"
+
+journalctl -u <jormungandr>.service -n 1 -f | grep --line-buffered 'stuck_notifier\|optionalsecondkeyword' | while read LINE
 do
   if [[ -n $LINE ]]; then
     NOW=$(date +"%Y-%m-%d_%H:%M:%S_%Z")  
-    Uptime=`/home/<USER>/.cargo/bin/jcli rest v0 node stats get -h http://127.0.0.1:<REST_PORT>/api | grep 'uptime'`
-    LastBlockHeight=`/home/<USER>/.cargo/bin/jcli rest v0 node stats get -h http://127.0.0.1:<REST_PORT>/api | grep 'lastBlockHeight'`
-    echo "Restarting due to stuck_notifier with $Uptime at $LastBlockHeight at $NOW" >> /home/<USER>/logs/stuck_check.log
-    systemctl stop <jormungandr.service>
-    sleep 5
-    systemctl start <jormungandr.service>
+    Uptime=`jcli rest v0 node stats get -h $REST_API | grep 'uptime'`
+    LastBlockHeight=`jcli rest v0 node stats get -h $REST_API | grep 'lastBlockHeight'`
+    if test -z "$Uptime"
+    then
+      echo "Node currently offline / bootstrapping, ignoring redundant restart logline"
+    else 
+      echo "Shutting down node due to stuck keyword with $Uptime at $LastBlockHeight at $NOW" 
+      jcli rest v0 shutdown get --host $REST_API
+    fi
   fi
-
-sleep 60
 done
